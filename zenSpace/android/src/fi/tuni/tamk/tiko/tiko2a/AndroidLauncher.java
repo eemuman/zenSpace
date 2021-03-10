84af3841f2e290543cad608818df10a3194af16f
Eemil Väänänen <eemil.vaananen@tuni.fi>
2021-03-10 16:05:39 +0200

    TYHJÄ PROJEKTI

---
+++ zenSpace/android/src/fi/tuni/tamk/tiko/tiko2a/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tuni.tamk.tiko.tiko2a;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tuni.tamk.tiko.tiko2a.zenSpace;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new zenSpace(), config);
+	}
+}
---
+++ zenSpace/desktop/src/fi/tuni/tamk/tiko/tiko2a/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tuni.tamk.tiko.tiko2a.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tuni.tamk.tiko.tiko2a.zenSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new zenSpace(), config);
+	}
+}

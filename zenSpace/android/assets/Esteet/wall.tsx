f638ecdad040d1b7182f40516a288cf50c28f688
Eemil Väänänen <eemil.vaananen@tuni.fi>
2021-04-06 12:59:09 +0300

    Päivitetyt atlakset

--- zenSpace/android/assets/Esteet/wall.tsx
+++ zenSpace/android/assets/Esteet/wall.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="wall" tilewidth="8" tileheight="8" tilecount="14220" columns="60">
- <image source="wall.png" width="480" height="1903"/>
+<tileset version="1.4" tiledversion="1.4.3" name="wall" tilewidth="8" tileheight="8" tilecount="15813" columns="63">
+ <image source="wall.png" width="511" height="2011"/>
 </tileset>

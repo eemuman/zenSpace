8d6c2cf9f026c651b5672172a34d11f3fa92a8a7
Petr Halinen <petr.halinen@tuni.fi>
2021-05-03 14:19:42 +0300

    Äänijuttuja.

--- zenSpace/core/src/fi/tuni/tamk/tiko/zenspace/Sounds.java
+++ zenSpace/core/src/fi/tuni/tamk/tiko/zenspace/Sounds.java
@@ -9,14 +9,25 @@ package fi.tuni.tamk.tiko.zenspace;
 
 import com.badlogic.gdx.audio.Music;
 
+/**
+ * This class is for handling all the sounds and music of the game.
+ */
 public class Sounds {
+    // Get BundleHandler from the class zenSpace
     private static BundleHandler handler = zenSpace.getBundle();
 
+    /**
+     * This method is used to load all the sound and music assets.
+     */
     public static void loadSounds() {
         handler.manager.load("audio/music/testmusic.mp3", Music.class);
         handler.manager.finishLoading();
     }
 
+    /**
+     * This method is used to be able to play the music in other classes.
+     * @return returns the Music class audiofile to be played.
+     */
     public static Music getMusic() {
         return handler.manager.get("audio/music/testmusic.mp3");
     }
